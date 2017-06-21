20a497cb97e0c2d5d01d677593b2cf59b6fdf3e5
LeqxLeqx <nonaya@biznez.com>
2017-06-21 15:20:34 -0700

    View / Controller integration

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   post	  '/signup',	to: 'users#create'
 
 
+  
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

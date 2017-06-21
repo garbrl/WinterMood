65c1bb3158a3a03d8497d4cb3251c8a55e5149ab
Vasundhara Gautam <vasundhara1997@gmail.com>
2017-06-21 14:58:38 -0700

    updated login / signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def new
+	  @user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			log_in @user
+	  	redirect_to @user
+		else
+		  render 'new'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+		def user_params
+				params.require(:user).permit(:username, :password)
+		end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  root    'welcome#index'
+
+  get     '/login',     to: 'sessions#new'
+  post    '/login',     to: 'sessions#create'
+  delete  '/logout',    to: 'sessions#destroy'
+  get     '/signup',    to: 'users#new'
+  post	  '/signup',	to: 'users#create'
+
+
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

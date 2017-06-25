a081af18d8b0d5a2253d45d71377f1f694e167c0
LeqxLeqx <nonaya@biznez.com>
2017-06-24 19:15:50 -0700

    controller mig

--- examples/login_example/test/controllers/login_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LoginControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get login_index_url
-    assert_response :success
-  end
-
-end
